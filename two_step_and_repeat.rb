e48d45d7a9ca55c67ca30640a8be820f819a403a
miathetortilla <mia.lamontagne@gmail.com>
2018-06-05 20:47:53 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -38,9 +38,17 @@ end
 end
 
 def how_many_steps?
-  
-  
-  
+  steps = 0
+loop do
+  steps += 1
+puts steps
+if steps % 2 == 0
+  puts "Left"
+  elsif
+  puts "Right"
+  sleep(0.5)
+end
+
   
   
   
